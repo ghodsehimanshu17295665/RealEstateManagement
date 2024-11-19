4aa3998ca8191e9d8674142cc97cc355af678766
Himanshu <himanshughodse@gmail.com>
2024-11-19 19:17:06 +0530

    Create Models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,61 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+
+from .managers import CustomUserManager
+
+
+class TimeStampeModel(models.model):
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        abstract = True
+
+
+class User(TimeStampeModel, AbstractUser):
+    ROLE_CHOICES = [
+        ('BUYER', 'Buyer'),
+        ('SELLER', 'Seller'),
+    ]
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="BUYER")
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
+
+class Property(TimeStampeModel):
+    STATUS_CHOICES = [
+        ('AVAILABLE', 'Available'),
+        ('SOLD', 'Sold'),
+        ('RENTED', 'Rented'),
+    ]
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="AVAILABLE")
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='properties')
+    image = models.ImageField(upload_to='static/', null=True, blank=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(TimeStampeModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
