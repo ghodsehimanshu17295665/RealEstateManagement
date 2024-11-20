82554b40535903ca3f2232ffb387a819a343f86e
Himanshu <himanshughodse@gmail.com>
2024-11-20 19:12:36 +0530

    Create Views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import views as auth_views
+from django.views.generic import DetailView, ListView, TemplateView, View
 
-# Create your views here.
+
+class Home(TemplateView):
+    template_name = "index.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
