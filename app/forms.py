903ca76f31191045f93dd5fe5449fd444d80f092
Himanshu <himanshughodse@gmail.com>
2024-11-22 18:42:46 +0530

    Create Form

--- app/forms.py
+++ app/forms.py
@@ -9,6 +9,14 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name", "email", "role", "password1", "password2"]
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your username'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your first name'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your last name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter your password'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm your password'}),
+        }
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -35,6 +43,31 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ("user", "birth_date", "gender")
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['user', 'birth_date', 'gender']  # Add your fields here
+
+    user = forms.ModelChoiceField(queryset=User.objects.all(), required=False)
+    profile_picture = forms.ImageField(required=False)
+    username = forms.CharField(required=True)
+    email = forms.EmailField(required=False)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+
+
+# Profile forms for Buyer and Seller
+class SellerForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = (
+            "user",
+            "birth_date",
+            "gender",
+        )
 
 
 class PropertyForm(forms.ModelForm):
