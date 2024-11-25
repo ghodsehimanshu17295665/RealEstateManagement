e3bdacc5a75744b1be08a5189175ed2271fa52a0
Himanshu <himanshughodse@gmail.com>
2024-11-25 18:42:11 +0530

    Create a Form

--- app/forms.py
+++ app/forms.py
@@ -46,16 +46,47 @@ class ProfileForm(forms.ModelForm):
 
 
 class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['user', 'birth_date', 'gender']  # Add your fields here
-
-    user = forms.ModelChoiceField(queryset=User.objects.all(), required=False)
-    profile_picture = forms.ImageField(required=False)
-    username = forms.CharField(required=True)
-    email = forms.EmailField(required=False)
+    # Fields from the User model
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    profile_picture = forms.ImageField(required=False)
+    phone_number = forms.CharField(required=False)
+    address = forms.CharField(widget=forms.Textarea, required=False)
+
+    class Meta:
+        model = Profile
+        fields = ['birth_date', 'gender']
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        if user:
+            # Pre-fill User fields
+            self.fields['first_name'].initial = user.first_name
+            self.fields['last_name'].initial = user.last_name
+            self.fields['email'].initial = user.email
+            self.fields['profile_picture'].initial = user.profile_picture
+            self.fields['phone_number'].initial = user.phone_number
+            self.fields['address'].initial = user.address
+
+    def save(self, commit=True):
+        profile = super().save(commit=False)
+        user = self.instance.user
+
+        # Save User fields
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+        user.profile_picture = self.cleaned_data['profile_picture']
+        user.phone_number = self.cleaned_data['phone_number']
+        user.address = self.cleaned_data['address']
+
+        if commit:
+            user.save()
+            profile.save()
+
+        return profile
 
 
 # Profile forms for Buyer and Seller
@@ -70,10 +101,14 @@ class SellerForm(forms.ModelForm):
         )
 
 
+# Property Form
 class PropertyForm(forms.ModelForm):
     class Meta:
         model = Property
         fields = ("title", "description", "price", "location", "status", "image", "seller", "category")
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 3}),
+        }
 
 
 class BookingForm(forms.ModelForm):
