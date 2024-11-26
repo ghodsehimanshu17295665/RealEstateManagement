665b1aae209ae220779356fa97414e7601a94294
Himanshu <himanshughodse@gmail.com>
2024-11-26 19:20:20 +0530

    Create urls

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,22 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import Home, SignUpView, LoginView, HouseView, LogoutView, BuyerDashboardView, SellerDashboardView, ProfileView, UpdateProfileView, Listing_View
+from .views import (
+    Home,
+    SignUpView,
+    LoginView,
+    HouseView,
+    LogoutView,
+    BuyerDashboardView,
+    SellerDashboardView,
+    ProfileView,
+    UpdateProfileView,
+    Listing_View,
+    PropertyList_View,
+    CreatePropertyView,
+    UpdatePropertyView,
+    DeletePropertyView,
+)
 
 urlpatterns = [
     # Home Page
@@ -10,12 +25,35 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login_page"),
     path("house/", HouseView.as_view(), name="house_page"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path('buyer-dashboard/', BuyerDashboardView.as_view(), name='buyer_dashboard'),
-    path('seller-dashboard/', SellerDashboardView.as_view(), name='seller_dashboard'),
-    path('profile/', ProfileView.as_view(), name='profile'),
-    path('profile/update/', UpdateProfileView.as_view(), name='update_profile'),
-    path('property/list/', Listing_View.as_view(), name='listing_view'),
-
-
+    path(
+        "buyer-dashboard/",
+        BuyerDashboardView.as_view(),
+        name="buyer_dashboard",
+    ),
+    path(
+        "seller-dashboard/",
+        SellerDashboardView.as_view(),
+        name="seller_dashboard",
+    ),
+    path("profile/", ProfileView.as_view(), name="profile"),
+    path(
+        "profile/update/", UpdateProfileView.as_view(), name="update_profile"
+    ),
+    path("property/list/", Listing_View.as_view(), name="listing_view"),
+    path("list/property/", PropertyList_View.as_view(), name="property_list"),
+    path(
+        "create/property/",
+        CreatePropertyView.as_view(),
+        name="create_property",
+    ),
+    path(
+        "property/update/<uuid:pk>/",
+        UpdatePropertyView.as_view(),
+        name="update_property",
+    ),
+    path("property/delete/<uuid:pk>/",
+         DeletePropertyView.as_view(),
+         name="delete_property",
+         ),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
