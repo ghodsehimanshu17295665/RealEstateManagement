625b565669c91e1886b6d811736348193b555f35
Himanshu <himanshughodse@gmail.com>
2024-12-03 14:46:44 +0530

    Create Urls

--- app/urls.py
+++ app/urls.py
@@ -23,7 +23,7 @@ from .views import (
     RemoveBookingView,
     ChangePasswoardSellerView,
     ChangePasswoardBuyerView,
-
+    ActivateAccountView,
 )
 
 urlpatterns = [
@@ -32,6 +32,12 @@ urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup_page"),
     path("login/", LoginView.as_view(), name="login_page"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    # email verification
+    path(
+        "activate/<uuid:uid>/<str:token>/",  # <uuid:uid> for UUID type
+        ActivateAccountView.as_view(),
+        name="activate",
+    ),
     path(
         "buyer-dashboard/",
         BuyerDashboardView.as_view(),
@@ -57,19 +63,42 @@ urlpatterns = [
         UpdatePropertyView.as_view(),
         name="update_property",
     ),
-    path("property/delete/<uuid:pk>/",
-         DeletePropertyView.as_view(),
-         name="delete_property",
-         ),
+    path(
+        "property/delete/<uuid:pk>/",
+        DeletePropertyView.as_view(),
+        name="delete_property",
+    ),
     path("all-properties/", AllPropertyView.as_view(), name="all_properties"),
     path("buyer/profile/", BuyerProfile.as_view(), name="buyer_profile"),
-    path("buyer/profile/update/", UpdateBuyerProfile.as_view(), name="update_buyer_profile"),
-    # path('properties/', AllPropertyView.as_view(), name='all_properties'),
-    path('properties/<uuid:pk>/add-booking/', AddBookingView.as_view(), name='add_booking'),
-    path('my-bookings/', BookingListView.as_view(), name='booking_list'),
-    path('property/<uuid:pk>/', PropertyDetailView.as_view(), name='property_details'),
-    path('remove-booking/<uuid:pk>/', RemoveBookingView.as_view(), name='remove_booking'),
-    path("change-password/", ChangePasswoardBuyerView.as_view(), name="change_password_buyer"),
-    path("change-password/", ChangePasswoardSellerView.as_view(), name="change_password_seller"),
-
+    path(
+        "buyer/profile/update/",
+        UpdateBuyerProfile.as_view(),
+        name="update_buyer_profile",
+    ),
+    path(
+        "properties/<uuid:pk>/add-booking/",
+        AddBookingView.as_view(),
+        name="add_booking",
+    ),
+    path("my-bookings/", BookingListView.as_view(), name="booking_list"),
+    path(
+        "property/<uuid:pk>/",
+        PropertyDetailView.as_view(),
+        name="property_details",
+    ),
+    path(
+        "remove-booking/<uuid:pk>/",
+        RemoveBookingView.as_view(),
+        name="remove_booking",
+    ),
+    path(
+        "change-password/",
+        ChangePasswoardBuyerView.as_view(),
+        name="change_password_buyer",
+    ),
+    path(
+        "change-password/",
+        ChangePasswoardSellerView.as_view(),
+        name="change_password_seller",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
